0bee50150aa3747ecc7cef42d97ef08267017c9a
ADJOfficial <ArsalanDaud098@gamil.com>
2024-04-13 00:08:12 +0500

    Initial Commit

---
+++ Dashboard/DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardApp.swift
+//  Dashboard
+//
+//  Created by ADJ on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
